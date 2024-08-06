17f1f004205208411a5d93b4369556f9d848d8c4
Wenhai-liu <Wenhai-liu@kubuntu.com>
2024-08-06 17:56:07 +0800

    v0.3

--- src/main.go
+++ src/main.go
@@ -22,14 +22,25 @@ func main() {
 func Run() {
 	cmd := exec.Command(os.Args[0], append([]string{"init"}, os.Args[2])...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
+		},
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
+	// 取消ns中的mount
+	//err := syscall.Unmount("rootfs/proc", syscall.MNT_FORCE)
+	//if err != nil {
+	//	panic(err)
+	//}
 }
 
 func Init() {
@@ -48,12 +59,12 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
-	err = syscall.Exec(os.Args[2], os.Args[2:], os.Environ())
-	if err != nil {
-		panic(err)
-	}
-	err = syscall.Unmount("/proc", syscall.MNT_FORCE)
-	if err != nil {
-		panic(err)
-	}
+	_ = syscall.Exec(os.Args[2], os.Args[2:], os.Environ())
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = syscall.Unmount("/proc", syscall.MNT_FORCE)
+	//if err != nil {
+	//	panic(err)
+	//}
 }
